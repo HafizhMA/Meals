77eb4e554cde89885dfc0819c01c5a59cd9e268c
HafizhMA <muhammadazhr29@gmail.com>
2023-08-25 14:57:22 +0700

    menambahkan image di jumbotron

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import jumbotronImg from "../assets/jumbotronimg.png";
 
 const Home = () => {
   return (
     <>
       <Header />
       <section id="jumbotron">
-        <div className="container d-flex flex-wrap">
+        <div className="container d-flex flex-wrap align-items-center justify-content-center">
           <div className="textjumbotron">
             <h2>choose the food you want</h2>
           </div>
-          <div className="imgjumbotron"></div>
+          <div className="imgjumbotron">
+            <img src={jumbotronImg} alt="" />
+          </div>
         </div>
       </section>
       <Footer />
