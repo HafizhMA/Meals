bbfe56f376adb93c34026f0bcdf562ba843342df
HafizhMA <muhammadazhr29@gmail.com>
2023-08-28 18:57:05 +0700

    nav link /

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <Navbar expand="lg" className="navbar">
       <Container>
-        <Navbar.Brand href="#home">Meals</Navbar.Brand>
+        <Navbar.Brand href="/">Meals</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
