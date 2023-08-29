922178adfe3185bd76dffbf962aff207b652de65
HafizhMA <muhammadazhr29@gmail.com>
2023-08-29 15:31:36 +0700

    add route detail meals page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import CategoriesPage from "./pages/CategoriesPage";
+import DetailMealsPage from "./pages/DetailMealsPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/categories/:strCategory" element={<CategoriesPage />} />
+      <Route path="/detail/:idMeal" element={<DetailMealsPage />} />
     </Routes>
   );
 }
