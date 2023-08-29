8dc6b06fbf9f2931dfb5b7d5287f4a6b81bfc5e8
HafizhMA <muhammadazhr29@gmail.com>
2023-08-29 15:44:42 +0700

    useEffect

--- src/components/DetailCardMeals.jsx
+++ src/components/DetailCardMeals.jsx
@@ -1,9 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Card from "react-bootstrap/Card";
-import { getCategoriesMeals } from "../api/api";
+import { getDetailMeals } from "../api/api";
 
 const DetailCardMeals = () => {
+  const { idMeal } = useParams();
+  const [detailMealsData, setDetailMealsData] = useState(null);
+
+  useEffect(() => {
+    async function fetchDetailMeals() {
+      try {
+        const detailMeals = await getDetailMeals(idMeal);
+        console.log(detailMeals);
+        setDetailMealsData(detailMeals);
+      } catch (error) {
+        console.log("error fetching", error);
+      }
+    }
+    fetchDetailMeals();
+  }, [idMeal]);
+
   return <div>DetailCardMeals</div>;
 };
 
