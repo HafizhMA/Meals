7fe783374d2d03f422d4dd76a4ff345bbb9d634c
HafizhMA <muhammadazhr29@gmail.com>
2023-08-29 15:39:59 +0700

    useparams, usestate

--- src/components/DetailCardMeals.jsx
+++ src/components/DetailCardMeals.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import { getCategoriesMeals } from "../api/api";
 
 const DetailCardMeals = () => {
   return <div>DetailCardMeals</div>;
