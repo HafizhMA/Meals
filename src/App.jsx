8f7f060848bd470b448776cf4737bc0a70e05846
HafizhMA <muhammadazhr29@gmail.com>
2023-09-06 18:35:45 +0700

    routing favorite page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import CategoriesPage from "./pages/CategoriesPage";
 import DetailMealsPage from "./pages/DetailMealsPage";
+import FavoritePage from "./pages/FavoritePage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/categories/:strCategory" element={<CategoriesPage />} />
       <Route path="/detail/:idMeal" element={<DetailMealsPage />} />
+      <Route path="/favorite/:idMeal" element={<FavoritePage />} />
     </Routes>
   );
 }
--- src/pages/FavoritePage.jsx
+++ src/pages/FavoritePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const FavoritePage = () => {
   return (
     <>
       <Header />
+      <h1>tes</h1>
       <Footer />
     </>
   );
