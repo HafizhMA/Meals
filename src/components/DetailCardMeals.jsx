e283c4580f15ad09a3845bdde2627fee984f2b5e
HafizhMA <muhammadazhr29@gmail.com>
2023-09-06 18:54:03 +0700

    heart fill onclick favorite

--- src/components/DetailCardMeals.jsx
+++ src/components/DetailCardMeals.jsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import { getDetailMeals } from "../api/api";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
 const DetailCardMeals = () => {
   const { idMeal } = useParams();
   const [detailMealsData, setDetailMealsData] = useState(null);
+  const [isHeartFilled, setIsHeartFilled] = useState(false);
 
   useEffect(() => {
     async function fetchDetailMeals() {
@@ -20,6 +22,10 @@ const DetailCardMeals = () => {
     fetchDetailMeals();
   }, [idMeal]);
 
+  const handleHeartClick = () => {
+    setIsHeartFilled(!isHeartFilled); // Toggle the state
+  };
+
   if (!detailMealsData) {
     return (
       <div className="text-center my-3">
@@ -29,37 +35,58 @@ const DetailCardMeals = () => {
   }
 
   return (
-    <section id="detailMealsCard" className="min-vh-100 my-5">
-      <div className="container">
-        <div className="row d-flex justify-content-center">
-          {detailMealsData.map((meal) => (
-            <div className="col" key={meal.idMeal}>
-              <Card id="card">
-                <Card.Img variant="top" src={meal.strMealThumb} />
-                <Card.Body>
-                  <Card.Title>{meal.strMeal}</Card.Title>
-                  <div className="d-flex flex-wrap justify-content-center">
-                    <Card.Text>Category: {meal.strCategory}</Card.Text>
-                    <Card.Text className="mx-4">Area: {meal.strArea}</Card.Text>
-                    <Card.Text>Tags: {meal.strTags}</Card.Text>
-                  </div>
-                  <Card.Text>{meal.strInstructions}</Card.Text>
-                  <div id="strYoutube" className="video-container">
-                    <iframe
-                      src={`https://www.youtube.com/embed/${
-                        meal.strYoutube.split("=")[1]
-                      }`}
-                      frameborder="0"
-                      allowfullscreen
-                    ></iframe>
-                  </div>
-                </Card.Body>
-              </Card>
-            </div>
-          ))}
+    <>
+      <section id="detailMealsCard" className="min-vh-100 my-5">
+        <div className="container">
+          <div className="row d-flex justify-content-center">
+            {detailMealsData.map((meal) => (
+              <div className="col" key={meal.idMeal}>
+                <Card id="card">
+                  <Card.Img variant="top" src={meal.strMealThumb} />
+                  <Card.Body>
+                    <Card.Title>{meal.strMeal}</Card.Title>
+                    <div className="d-flex flex-wrap justify-content-center">
+                      <Card.Text>Category: {meal.strCategory}</Card.Text>
+                      <Card.Text className="mx-4">
+                        Area: {meal.strArea}
+                      </Card.Text>
+                      <Card.Text>Tags: {meal.strTags}</Card.Text>
+                    </div>
+                    <Card.Text>{meal.strInstructions}</Card.Text>
+                    <div id="strYoutube" className="video-container">
+                      <iframe
+                        src={`https://www.youtube.com/embed/${
+                          meal.strYoutube.split("=")[1]
+                        }`}
+                        frameborder="0"
+                        allowfullscreen
+                      ></iframe>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </div>
+            ))}
+          </div>
         </div>
+      </section>
+      <div className="d-flex flex-row-reverse">
+        {isHeartFilled ? (
+          <AiFillHeart
+            id="heartFill"
+            className="heartFillPosition"
+            size={50}
+            onClick={handleHeartClick}
+          />
+        ) : (
+          <AiOutlineHeart
+            id="heart"
+            className="heartPosition"
+            size={50}
+            onClick={handleHeartClick}
+          />
+        )}
       </div>
-    </section>
+    </>
   );
 };
 
